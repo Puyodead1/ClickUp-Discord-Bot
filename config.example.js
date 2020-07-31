ee8f3cbcefb5792577c4a9c6a548030f076f3e7e
Puyodead1 <23562356riley@gmail.com>
2020-07-31 12:32:49 -0400

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  TOKEN: "super secret bot token",
+  PREFIX: "cu!",
+  STAFF_ROLE: "role for any staff that should be able to use the bot",
+  CLICKUP_TOKEN: "clickup personal access token", // you could (and probably should) make a "dummy" account for the bot
+  TEAM_ID: "team id",
+  SPACE_ID: "space id",
+};

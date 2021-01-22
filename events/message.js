a16fed4d45682b18e77eddf11f72c936755976b3
Puyodead1 <23562356riley@gmail.com>
2021-01-22 12:34:39 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ module.exports = async (client, message) => {
   if (message.channel.type !== "text") return;
   if (!message.content.toLowerCase().startsWith(config.PREFIX.toLowerCase())) return;
   if (!message.member.roles.cache.has(config.STAFF_ROLE))
-    return message.channel.send("Im tryna sleep hoe");
+    return message.channel.send("no permission");
 
   const args = message.content.slice(config.PREFIX.length).trim().split(/ +/g);
   const cmdName = args.shift().toLowerCase();
